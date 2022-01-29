ab90d1fcc67dec634b0fc100e06e15e84a819a98
Md Sujon <92178630+sujon111111111@users.noreply.github.com>
2022-01-29 19:33:39 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+var typed = new Typed(".typing2",{
+    strings:["","Web Designer","sujon"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
